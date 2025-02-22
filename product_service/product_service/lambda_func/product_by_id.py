75607adf980f54dea90eaf07845796a1e751fd42
Olga <helga.prikhodko@gmail.com>
2025-02-22 17:09:00 +0200

    fix: update product id type from string to number in mock data

--- product_service/product_service/lambda_func/product_by_id.py
+++ product_service/product_service/lambda_func/product_by_id.py
@@ -3,9 +3,9 @@ import json
 
 def handler(event, _context):
     products = [
-        {"id": 1, "title": "Citrus Calamondin Grafted Trellis", "price": 29.90},
-        {"id": 2, "title": "European Fan Palm - Chamaerops humilis", "price": 19.99},
-        {"id": 3, "title": "Dipsis - Dipsis lutescens", "price": 3.49},
+        {"id": "1", "title": "Citrus Calamondin Grafted Trellis", "price": 29.90},
+        {"id": "2", "title": "European Fan Palm - Chamaerops humilis", "price": 19.99},
+        {"id": "3", "title": "Dipsis - Dipsis lutescens", "price": 3.49},
     ]
 
     # Extract the product ID from the path parameters
