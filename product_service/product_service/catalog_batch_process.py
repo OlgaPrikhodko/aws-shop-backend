2deb3c102e311a341e856ab3823e8d7139447077
Olga <helga.prikhodko@gmail.com>
2025-03-15 14:02:40 +0200

    🌟 feat(sns): integrate SNS notifications for batch product processing

--- product_service/product_service/catalog_batch_process.py
+++ product_service/product_service/catalog_batch_process.py
@@ -2,7 +2,10 @@ from aws_cdk import (
     Stack,
     aws_lambda as lambda_,
     aws_sqs as sqs,
-    aws_lambda_event_sources as lambda_event_sources)
+    aws_lambda_event_sources as lambda_event_sources,
+    aws_sns as sns,
+    aws_sns_subscriptions as sns_subscriptions
+)
 
 from constructs import Construct
 
@@ -20,6 +23,15 @@ class CatalogBatchProcess(Stack):
         event_source = lambda_event_sources.SqsEventSource(
             catalog_items_queue, batch_size=5)
 
+        create_product_topic = sns.Topic(
+            self, "CreateProductTopic", topic_name="create_product_topic")
+
+        create_product_topic.add_subscription(
+            sns_subscriptions.EmailSubscription("ggg.poletaem@gmail.com"))
+
+        environment["SNS_TOPIC_ARN"] = create_product_topic.topic_arn
+
+        # Create Lambda function
         self.catalog_batch_process = lambda_.Function(
             self, "CatalogBatchProcess",
             runtime=lambda_.Runtime.PYTHON_3_12,
@@ -32,3 +44,6 @@ class CatalogBatchProcess(Stack):
         self.catalog_batch_process.add_event_source(event_source)
         catalog_items_queue.grant_consume_messages(
             self.catalog_batch_process)
+
+        # SNS policy
+        create_product_topic.grant_publish(self.catalog_batch_process)
--- product_service/product_service/lambda_func/catalog_batch.py
+++ product_service/product_service/lambda_func/catalog_batch.py
@@ -2,13 +2,18 @@ import json
 import os
 import boto3
 
+
 dynamodb_client = boto3.client('dynamodb')
 dynamodb = boto3.resource("dynamodb", region_name=os.getenv("AWS_REGION"))
+sns_client = boto3.client('sns')
 
 
 def handler(event, _context):
     stock_table_name = os.environ['STOCK_TABLE_NAME']
     product_table_name = os.environ['PRODUCTS_TABLE_NAME']
+    sns_topic_arn = os.environ['SNS_TOPIC_ARN']
+
+    products_for_sns = []
 
     for record in event['Records']:
         record = json.loads(record['body'])
@@ -34,6 +39,15 @@ def handler(event, _context):
             'count': {'N': str(record['count'])}
         }
 
+        sns_product = {
+            'id': str(record['id']),
+            'title': record['title'],
+            'description': record['description'],
+            'price': record['price'],
+            'count': record['count']
+        }
+        products_for_sns.append(sns_product)
+
         # save to DynamoDB
         dynamodb_client.transact_write_items(
             TransactItems=[
@@ -51,6 +65,21 @@ def handler(event, _context):
                 }
             ]
         )
+
+        sns_message = {
+            'default': json.dumps({
+                'message': 'New roduct added',
+                'products': products_for_sns
+            })
+        }
+
+        response = sns_client.publish(
+            TopicArn=sns_topic_arn,
+            Message=json.dumps(sns_message),
+            MessageStructure='json'
+        )
+
+        print(f"Message sent to SNS topic: {response['MessageId']}")
 
         return {
             'statusCode': 200,
