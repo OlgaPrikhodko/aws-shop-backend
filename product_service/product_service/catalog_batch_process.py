4607f48858d5e23f465b2654c8eecba1b6e7c0ba
Olga <helga.prikhodko@gmail.com>
2025-03-15 20:24:36 +0200

    :bookmark_tabs: docs: add documentation for CatalogBatchProcess

--- product_service/product_service/catalog_batch_process.py
+++ product_service/product_service/catalog_batch_process.py
@@ -11,26 +11,38 @@ from constructs import Construct
 
 
 class CatalogBatchProcess(Stack):
+    """
+    AWS CDK Stack for catalog batch processing infrastructure.
+
+    This stack creates:
+    - SQS Queue for receiving product data
+    - SNS Topic for notifications with filtered subscriptions
+    - Lambda function for processing the data
+    - Necessary IAM permissions and event sources
+    """
+
     def __init__(self, scope: Construct, construct_id: str, environment: dict, **kwargs):
         super().__init__(scope, construct_id, **kwargs)
 
-        # SQS implementation
+        # Create SQS Queue for receiving product data
         catalog_items_queue = sqs.Queue(
             self,
             "CatalogItemsQueue"
         )
 
+        # Configure SQS as event source for Lambda
         event_source = lambda_event_sources.SqsEventSource(
             catalog_items_queue, batch_size=5)
 
-        # Subscription for all products
+        # Create SNS Topic for notifications
         create_product_topic = sns.Topic(
             self, "CreateProductTopic", topic_name="create_product_topic")
 
+        # Add email subscription for all products (no filter)
         create_product_topic.add_subscription(
             sns_subscriptions.EmailSubscription("ggg.poletaem@gmail.com"))
 
-        # Subscription for expensive products (price > 100)
+        # Add filtered subscription for expensive products (price > 100)
         create_product_topic.add_subscription(
             sns_subscriptions.EmailSubscription(
                 "helga.prikhodko@gmail.com",
@@ -42,9 +54,10 @@ class CatalogBatchProcess(Stack):
             )
         )
 
+        # Add SNS Topic ARN to Lambda environment variables
         environment["SNS_TOPIC_ARN"] = create_product_topic.topic_arn
 
-        # Create Lambda function
+        # Create Lambda function for processing catalog items
         self.catalog_batch_process = lambda_.Function(
             self, "CatalogBatchProcess",
             runtime=lambda_.Runtime.PYTHON_3_12,
@@ -54,9 +67,11 @@ class CatalogBatchProcess(Stack):
         )
 
         # SQS policy
+        # Grant Lambda permission to consume messages from SQS
         self.catalog_batch_process.add_event_source(event_source)
         catalog_items_queue.grant_consume_messages(
             self.catalog_batch_process)
 
         # SNS policy
+        # Grant Lambda permission to publish to SNS
         create_product_topic.grant_publish(self.catalog_batch_process)
